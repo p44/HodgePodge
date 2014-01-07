dd1466bf3b3fc251c50f407273fef67e3177acfe
mwilson <mwilson@processor44.com>
2014-01-06 17:29:59 -0700

    added akka test kit and eh cache play plugin

--- project/Build.scala
+++ project/Build.scala
@@ -7,7 +7,10 @@ object ApplicationBuild extends Build {
   val appName         = "HodgePodge"
   val appVersion      = "1.0"
  
-  val appDependencies = Nil
+  val appDependencies = Seq(
+      "com.typesafe.akka" %% "akka-testkit" % "2.2.1" % "test",
+      cache
+  )
  
   val main = play.Project(appName, appVersion, appDependencies).settings(
     scalaVersion := "2.10.2"
