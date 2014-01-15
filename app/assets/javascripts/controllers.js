cbd248e7c7219df17da48149b4d395574547f2f9
mwilson <mwilson@processor44.com>
2014-01-15 09:36:49 -0700

    Added EventSource listener for the fish store two feed, changed the data structure of what is returned from post deliver to match DeliveryReceipt

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -11,7 +11,6 @@ controller('FishStoreOneCtrl',function ($scope, $http, $location, $timeout) {
 	$scope.delivery_result_pending = JSON.parse('{"message": "...", "time": "..."}');
 	$scope.delivery_result_error = JSON.parse('{"message": "Error", "time": ""}');
 	$scope.delivery_result = $scope.delivery_result_none;
-	$scope.callback_msgs = [];
 	
 	$scope.catchExists = function() {
 		return $scope.latest_catch_size > 0;
@@ -67,11 +66,12 @@ controller('FishStoreTwoCtrl',function ($scope, $http, $location, $timeout) {
 	$scope.latest_catch = $scope.latest_catch_empty; // this will be modified with $$hashkey
 	$scope.latest_catch_raw = $scope.latest_catch_empty;
 	$scope.latest_catch_size = 0;
-	$scope.delivery_result_none = JSON.parse('{"message": "", "time": ""}');
-	$scope.delivery_result_pending = JSON.parse('{"message": "...", "time": "..."}');
-	$scope.delivery_result_error = JSON.parse('{"message": "Error", "time": ""}');
+	// DeliveryReceipt(id: Long, fishCount: Int, totalWeight: Double, payment: Double, time: String)
+	$scope.delivery_result_none = JSON.parse('{"id": 0, "fishCount": 0, "totalWeight": 0, "payment": 0, "time": "", "message": ""}');
+	$scope.delivery_result_pending = JSON.parse('{"id": 0, "fishCount": 0, "totalWeight": 0, "payment": 0, "time": "...", "message": ""}');
+	$scope.delivery_result_error = JSON.parse('{"id": 0, "fishCount": 0, "totalWeight": 0, "payment": 0, "time": "", "message": "Error"}');
 	$scope.delivery_result = $scope.delivery_result_none;
-	$scope.callback_msgs = [];
+	$scope.delivery_feed_msgs = []; // array of dropped fish
 	
 	$scope.catchExists = function() {
 		return $scope.latest_catch_size > 0;
@@ -117,4 +117,21 @@ controller('FishStoreTwoCtrl',function ($scope, $http, $location, $timeout) {
 			  $scope.delivery_result = $scope.delivery_result_error;
 	    });
 	}
+	
+	/** handle incoming delivery feed messages: add to messages array */
+    $scope.addDeliveryFeedMsg = function (msg) { 
+    	var msgobj = JSON.parse(msg.data);
+    	console.log('Got DeliveryFeedMsg' + msg.data);
+        $scope.$apply(function () { 
+        	$scope.delivery_feed_msgs.push(msgobj); // add to array
+        });
+    };
+    
+	/** start listening to the deliery feed for the fish store */
+    $scope.listen = function () {
+    	$scope.delivery_feed = new EventSource("/feed/fish_store_two/delivery"); 
+        $scope.delivery_feed.addEventListener("message", $scope.addDeliveryFeedMsg, false);
+    };
+    
+    $scope.listen();
 });
